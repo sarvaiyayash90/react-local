87055c38d7c20adafedadb9f147610f4b247c5e0
sarvaiyayash90 <sarvaiyayash90@gmail.com>
2021-07-13 17:03:17 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './style.css';
 // import Myname, { NewApp, NewApp_new } from './NewApp';
 import * as MYdata from './NewApp';
@@ -31,8 +31,9 @@ const allData = [
 
 export default function App() {
   const [data, setdata] = useState([]);
-
   const [newname, setname] = useState('yash');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const setNameHandler = name => {
     // console.log('name is ', name);
@@ -47,20 +48,51 @@ export default function App() {
     setdata(dd);
   };
 
-  const fetchData = async () => {
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
-    const newData = data.results.map(item => {
-      return {
-        id: item.episode_id,
-        title: item.title,
-        openingText: item.opening_crawl,
-        releaseDate: item.release_date
-      };
-    });
-    // console.log('dassasas', newData);
-    setdata(newData);
-  };
+  const fetchDataHandler = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      // const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        'https://react-demo-bc807-default-rtdb.firebaseio.com/movies.json'
+      );
+      if (!response.ok) {
+        throw new Error('Something Went Wrong!');
+      }
+      const data = await response.json();
+      const newData = data.results.map(item => {
+        return {
+          id: item.episode_id,
+          title: item.title,
+          openingText: item.opening_crawl,
+          releaseDate: item.release_date
+        };
+      });
+      // console.log('dassasas', newData);
+      setdata(newData);
+    } catch (error) {
+      setError(error.message);
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchDataHandler();
+  }, [fetchDataHandler]);
+
+  let content = <p>found No data...</p>;
+
+  if (data.length > 0) {
+    content = <List items={data} removeData={remove} />;
+  }
+
+  if (loading) {
+    content = <p>loading...</p>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
 
   return (
     <div>
@@ -68,12 +100,20 @@ export default function App() {
       <MYdata.NewApp_new />
       <MYdata.NewApp />
       {MYdata.datadata}
-      <List items={data} removeData={remove} />
       <NewData onAdddata={setNameHandler} />
       &nbsp;&nbsp;{newname}
       <br />
       <br />
-      <button onClick={() => fetchData()}>fetch data</button>
+      <button onClick={() => fetchDataHandler()}>fetch data</button>
+      {content}
+      {/* {!loading && data.length > 0 && <List items={data} removeData={remove} />}
+      {!loading && data.length === 0 && !error && <h1>No data found </h1>}
+      {!loading && error && <p>{error}</p>}
+      {loading && (
+        <h1>
+          <b> Loading Data Please wait... </b>
+        </h1>
+      )} */}
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
